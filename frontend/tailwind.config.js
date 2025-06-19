c969c0eda485901d74a1e2c8cb15bae4dc3461bb
unknown <sanika2003.patil@gmail.com>
2025-06-19 17:55:18 +0530

    tailwind setup

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        indigo: {
+          800: '#1C2536',
+          700: '#2d3748'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
